10d476ebcfdf8d2cd2c55f33bcd3b25cf9cf35a3
Harry <harryread@outlook.com>
2021-04-24 18:07:41 +0100

    1st commit

--- Reflex-Logging.cpp
+++ Reflex-Logging.cpp
@@ -4,27 +4,23 @@
 
 #include <utility>
 
-//BOOL APIENTRY DllMain( HMODULE hModule,
-//                       DWORD  ul_reason_for_call,
-//                       LPVOID lpReserved
-//                     )
-//{
-//    return TRUE;
-//}
-//
 
 
 Reflex::Log::Logger::Logger(const std::string& name)
 {
     m_loggerName = name;
-    m_logger = spdlog::stdout_color_mt(name);
+    m_logger = spdlog::get(name);
+    if (!m_logger) 
+    {
+        
+        m_logger = spdlog::stdout_color_mt(name);
+    }
 }
 
 Reflex::Log::Logger::Logger(std::shared_ptr<spdlog::logger> pLogger)
 {
     m_logger = std::move(pLogger);
 }
-
 
 void Reflex::Log::Logger::testLog() const
 {
@@ -35,7 +31,54 @@ std::string Reflex::Log::Logger::getLoggerName() const {
     return m_loggerName;
 }
 
-//APPLIB_EXPORT void Reflex::Log::registerLogger(std::shared_ptr<spdlog::logger> logger)
+void Reflex::Log::shutdown()
+{
+    spdlog::shutdown();
+}
+
+void Reflex::Log::setDefaultLog(const std::string& logName)
+{
+    auto log = spdlog::get(logName);
+    if (!log)
+        std::make_shared<Reflex::Log::Logger>("Default-Log");
+
+    spdlog::set_default_logger(spdlog::get(logName));
+}
+
+void Reflex::Log::removeLog(const std::string& logName)
+{
+    if(spdlog::get(logName))
+        spdlog::drop(logName);
+}
+
+std::shared_ptr<Reflex::Log::Logger> Reflex::Log::createNewLogger(const std::string& logName)
+{
+    using Reflex::Log::Logger;
+
+    return std::make_shared<Logger>(logName);
+    /*if(logMap.empty())
+    {
+        auto newLog = std::make_shared<Logger>(logName);
+        return logMap.try_emplace(logName, newLog).first->second;
+    }
+
+    for(auto t = logMap.begin(); t != logMap.end(); ++t)
+    {
+        if(t->first == logName)
+        {
+            return t->second;
+        }
+
+        if(t == logMap.end() || logMap.size() == 0)
+        {
+            auto newLog = std::make_shared<Logger>(logName);
+            return logMap.try_emplace(logName, newLog).first->second;
+        }
+    }
+    return nullptr;*/
+}
+
+//std::shared_ptr<Reflex::Log::Logger> Reflex::Log::LogRegister::getLoggerByName (const std::string& logName)
 //{
-//    spdlog::register_logger(logger);
+//    return logMap[logName];
 //}
--- Reflex-Logging.h
+++ Reflex-Logging.h
@@ -10,69 +10,75 @@
 #include <spdlog/sinks/stdout_color_sinks.h>
 #include <spdlog/sinks/stdout_sinks.h>
 
-#define APPLIB_EXPORT
-
 namespace Reflex::Log
 {
-
-
-    class APPLIB_EXPORT Logger
+    class Logger
     {
 
-    private:
+    protected:
         std::shared_ptr<spdlog::logger> m_logger;
         std::string m_loggerName = "NotSet";
+
 
     public:
         Logger() = delete;
         Logger(const std::string& name);
-        Logger(std::shared_ptr<spdlog::logger> pLogger);
+        Logger(std::shared_ptr<spdlog::logger> logName);
         ~Logger() = default;
-        void testLog() const;
 
 
+        void  testLog() const;
         std::string getLoggerName() const;
 
-
         template <typename... Args>
-        void writeWarning(std::string message, Args ... arguments)
+        void writeWarning(const std::string& message, Args ... arguments)
         {
             m_logger->warn(message, std::forward<Args>(arguments)...);
         }
 
         template <typename... Args>
-        void writeNotice(std::string message, Args ... arguments)
+        void writeNotice(const std::string& message, Args ... arguments)
         {
             m_logger->info(message, std::forward<Args>(arguments)...);
         }
 
         template <typename... Args>
-        void writeError(std::string message, Args ... arguments)
+        void writeError(const std::string& message, Args ... arguments)
         {
             m_logger->error(message, std::forward<Args>(arguments)...);
         }
 
         template <typename... Args>
-        void writeDebug(std::string message, Args ... arguments)
+        void writeDebug(const std::string& message, Args ... arguments)
         {
             m_logger->debug(message, std::forward<Args>(arguments)...);
         }
 
         template <typename... Args>
-        void writeCritical(std::string message, Args ... arguments)
+        void writeCritical(const std::string& message, Args ... arguments)
         {
             m_logger->critical(message, std::forward<Args>(arguments)...);
         };
 
         template <typename... Args>
-        void writeTrace(std::string message, Args ... arguments)
+        void writeTrace(const std::string& message, Args ... arguments)
         {
             m_logger->trace(message, std::forward<Args>(arguments)...);
         }
+
+
     };
     //improve
     // Replace gets()-> with pre-created var
 
-    APPLIB_EXPORT void registerLogger(std::shared_ptr<spdlog::logger> logger);
+    void shutdown();
+    void setDefaultLog(const std::string& logName);
+    void removeLog(const std::string& logName);
+    std::shared_ptr<Reflex::Log::Logger> createNewLogger(const std::string& logName);
+    //static std::shared_ptr<Reflex::Log::Logger> getLoggerByName(const std::string& logName);
+
+
+
+
 
 }
