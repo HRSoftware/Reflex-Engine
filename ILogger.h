14fe3409cd1eb9cbfdff7c17a6c12fe0b21877f6
Harry <harryread@outlook.com>
2021-04-24 19:09:06 +0100

    Removed ignored files

---
+++ ILogger.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <string>
+
+//template<typename T>
+//class ILogger
+//{
+//    public:
+//    template<typename... Args>
+//    void writeErrorT(const std::string&, ...);
+//};
+
